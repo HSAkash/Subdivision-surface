cb60a61f0188ef0909b9100b54d5d251fe7069a9
Hemel Sharker Akash <44114317+HSAkash@users.noreply.github.com>
2023-11-22 18:34:54 +0600

    Update main_vedo.py

--- main_vedo.py
+++ main_vedo.py
@@ -1,11 +1,18 @@
 import trimesh
 from cmc import cmc_subdiv
-from vedo import trimesh2vedo, show, Line, Plotter, Points
+from vedo import trimesh2vedo, show, Line, Plotter, Points, Lines
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def plot_mesh(vertices, faces,edge_plot_bool, cmap="magma", edge_color='black', edge_width=4, point_color='green', point_size=5):
+def plot_mesh(vertices,
+              faces,edge_plot_bool,
+              cmap="magma",
+              edge_color='black',
+              edge_width=4,
+              point_color='green',
+              point_size=5,
+              only_edge=False):
     meshe = trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
     vmeshes = trimesh2vedo(meshe)
     # Define some dummy point scalar
@@ -16,17 +23,17 @@ def plot_mesh(vertices, faces,edge_plot_bool, cmap="magma", edge_color='black', 
     # Create a Plotter object
     plotter = Plotter()
 
-    # Add the mesh to the plotter
-    plotter.add(vmeshes)
+    if not only_edge:
+        # Add the mesh to the plotter
+        plotter.add(vmeshes)
 
     # Add edges to the plot
-    print(f"________________________Edge Ploting Started___________________________")
     if edge_plot_bool:
+        print(f"________________________Edge Ploting Started___________________________")
         edges = meshe.edges
-        for edge in tqdm(edges):
-            p1, p2 = meshe.vertices[edge]
-            plotter.add(Line(p1, p2, c=edge_color, lw = edge_width))
-
+        lines = Lines(meshe.vertices[edges], c=edge_color, lw=edge_width)
+        plotter.add(lines)
+        print(f"________________________Edge Ploting End___________________________")
         # Add vertices to the plot
         vertices = Points(meshe.vertices, r=point_size, c=point_color)
         plotter.add(vertices)
@@ -48,7 +55,8 @@ def plot_mesh_with_initial(
         initial_edge_color='black',
         initial_edge_width=4,
         initial_point_color='green',
-        initial_point_size=5):
+        initial_point_size=5,
+        only_edge=False):
     meshe = trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
     vmeshes = trimesh2vedo(meshe)
     # Define some dummy point scalar
@@ -59,30 +67,29 @@ def plot_mesh_with_initial(
     # Create a Plotter object
     plotter = Plotter()
 
-    # Add the mesh to the plotter
-    plotter.add(vmeshes)
+    if not only_edge:
+        # Add the mesh to the plotter
+        plotter.add(vmeshes)
 
     # Add edges to the plot
     if edge_plot_bool:
         print(f"________________________Edge Ploting Started___________________________")
         edges = meshe.edges
-        for edge in tqdm(edges):
-            p1, p2 = meshe.vertices[edge]
-            plotter.add(Line(p1, p2, c=edge_color, lw = edge_width))
+        lines = Lines(meshe.vertices[edges], c=edge_color, lw=edge_width)
+        plotter.add(lines)
+        
 
         # Add vertices to the plot
         vertices = Points(meshe.vertices, r=point_size, c=point_color)
         plotter.add(vertices)
 
-        # initial mesh
         initial_edge = initial_mesh.edges
-        for edge in tqdm(initial_edge):
-            p1, p2 = initial_mesh.vertices[edge]
-            plotter.add(Line(p1, p2, c=initial_edge_color, lw = initial_edge_width))
+        lines = Lines(initial_mesh.vertices[initial_edge], c=initial_edge_color, lw=initial_edge_width)
+        plotter.add(lines)
 
         initial_vertices = Points(initial_mesh.vertices, r=initial_point_size, c=initial_point_color)
         plotter.add(initial_vertices)
-
+        print(f"________________________Edge Ploting End___________________________")
 
     # Show the plot with no axes or background grid
     plotter.show(axes=None)
@@ -102,9 +109,13 @@ if __name__ == '__main__':
     point_color = input("Enter point color: ")
     point_size = int(input("Enter point size: "))
     edge_plot_bool = input("Show edge plot: y/n: ")
-    edge_plot_bool = True if edge_plot_bool.lower()=='y' else False
+    only_edge = input("Only edge plot: y/n: ")
     initial_mesh = input("Show initial mesh edge: y/n: ")
     initial_mesh = initial_mesh.lower()
+
+    
+    edge_plot_bool = True if edge_plot_bool.lower()=='y' else False
+    only_edge = True if only_edge.lower()=='y' else False
 
     if initial_mesh=='y':
         initial_mesh_edge_color = input("Enter initial mesh edge color: ")
@@ -136,7 +147,10 @@ if __name__ == '__main__':
         output_points, output_faces = cmc_subdiv(output_points, output_faces)
     print(f"________________________Iteration Completed___________________________")
 
-
+    print(f"""
+Total number of new vertices: {len(output_points)}
+Total number of new faces: {len(output_faces)}
+    """)
     # plot_mesh(output_points, output_faces, cmap=cmap, edge_color=edge_color, edge_width=edge_width, point_color=point_color, point_size=point_size)
     if initial_mesh == 'y':
         for cmap in plt.colormaps():
@@ -153,8 +167,24 @@ if __name__ == '__main__':
                 initial_edge_color=initial_mesh_edge_color,
                 initial_edge_width=initial_mesh_edge_width,
                 initial_point_color=initial_mesh_point_color,
-                initial_point_size=initial_mesh_point_size
+                initial_point_size=initial_mesh_point_size,
+                only_edge=only_edge
             )
+            
+            if only_edge:
+                break
     else:
         for cmap in plt.colormaps():
-            plot_mesh(output_points, output_faces,edge_plot_bool, cmap=cmap, edge_color=edge_color, edge_width=edge_width, point_color=point_color, point_size=point_size)
+            plot_mesh(
+                output_points,
+                output_faces,
+                edge_plot_bool,
+                cmap=cmap,
+                edge_color=edge_color,
+                edge_width=edge_width,
+                point_color=point_color,
+                point_size=point_size,
+                only_edge=only_edge
+            )
+            if only_edge:
+                break
